6f4527b15f1d1b4c5f912a5993375f39f9968dd6
James Dibble <james@jdibble.co.uk>
2014-09-28 18:31:21 +0100

    Add icon and make sure the app always runs as admin

--- ChocolateyExplorer.WPF/App.xaml.cs
+++ ChocolateyExplorer.WPF/App.xaml.cs
@@ -1,17 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace ChocolateyExplorer.WPF
+﻿namespace ChocolateyExplorer.WPF
 {
+    using System;
+    using System.Deployment.Application;
+    using System.Diagnostics;
+    using System.Reflection;
+    using System.Security.Principal;
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this.Startup += this.Application_Startup;
+        }
+
+        private bool IsRunAsAdministrator()
+        {
+            var wi = WindowsIdentity.GetCurrent();
+            var wp = new WindowsPrincipal(wi);
+
+            return wp.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (!IsRunAsAdministrator())
+            {
+                var processInfo = new ProcessStartInfo(Assembly.GetExecutingAssembly().CodeBase)
+                {
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+
+                try
+                {
+                    Process.Start(processInfo);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Sorry, this application must be run as Administrator.");
+                }
+
+                this.Shutdown();
+            }
+        }
     }
 }
