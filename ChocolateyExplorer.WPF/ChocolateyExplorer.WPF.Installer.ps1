c265adbcd82b9c77d6be3300827e6e5e75d8e0c2
James Dibble <james@jdibble.co.uk>
2014-10-05 17:39:53 +0100

    Moving Chocolatey package specifications

---
+++ ChocolateyExplorer.WPF/ChocolateyExplorer.WPF.Installer.ps1
@@ -0,0 +1,7 @@
+$packageName = 'Chocolatey Explorer'
+$installerType = 'exe'
+$url = 'http://chocolatey-explorer.jdibble.co.uk/setup.exe'
+$silentArgs = '/i /s'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
