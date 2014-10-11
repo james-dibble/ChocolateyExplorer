64946d1fe8b5ae819c47c36a47f8dd619cd6a643
James Dibble <james@jdibble.co.uk>
2014-10-11 12:39:26 +0100

    Go back to using the setup executable instead of the application manifest.

--- ChocolateyExplorer.WPF/ChocolateyExplorer.WPF.Installer.ps1
+++ ChocolateyExplorer.WPF/ChocolateyExplorer.WPF.Installer.ps1
@@ -1,7 +1,7 @@
 $packageName = 'Chocolatey Explorer'
 $installerType = 'exe'
-$url = 'http://chocolatey-explorer.jdibble.co.uk/ChocolateyExplorer.application'
-$silentArgs = ''
+$url = 'http://chocolatey-explorer.jdibble.co.uk/setup.exe'
+$silentArgs = '/i /s'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
