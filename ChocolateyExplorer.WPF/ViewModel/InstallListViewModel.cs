fe736fb0c2d6febe04b96b5edb38443ab6e8c22f
James Dibble <james@jdibble.co.uk>
2014-10-26 20:04:34 +0000

    Adding remove button to install list

--- ChocolateyExplorer.WPF/ViewModel/InstallListViewModel.cs
+++ ChocolateyExplorer.WPF/ViewModel/InstallListViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace ChocolateyExplorer.WPF.ViewModel
 {
     using System.Collections.ObjectModel;
+    using System.Linq;
     using System.Windows.Input;
 
     using Chocolatey.DomainModel;
@@ -10,31 +11,52 @@
 
     public class InstallListViewModel : ViewModelBase
     {
+        private ChocolateyPackageVersion _selectedPackage;
+
         public InstallListViewModel()
         {
             this.Packages = new ObservableCollection<ChocolateyPackageVersion>();
 
             this.AddPackageToInstallListCommand = new RelayCommand<ChocolateyPackageVersion>(this.AddPackageToInstallList);
-            this.RemovePackageFromInstallListCommand = new RelayCommand<ChocolateyPackageVersion>(this.RemovePackageToInstallList);
-            this.ClearInstallListCommand = new RelayCommand(() => this.Packages.Clear());
+            this.RemovePackageFromInstallListCommand = 
+                new RelayCommand<ChocolateyPackageVersion>(this.RemovePackageToInstallList, package => package != null);
+            this.ClearInstallListCommand = new RelayCommand(() => this.Packages.Clear(), () => this.Packages.Any());
         }
 
         public ObservableCollection<ChocolateyPackageVersion> Packages { get; private set; }
 
         public ICommand AddPackageToInstallListCommand { get; private set; }
 
-        public ICommand RemovePackageFromInstallListCommand { get; private set; }
+        public RelayCommand<ChocolateyPackageVersion> RemovePackageFromInstallListCommand { get; private set; }
 
-        public ICommand ClearInstallListCommand { get; private set; }
+        public RelayCommand ClearInstallListCommand { get; private set; }
+
+        public ChocolateyPackageVersion SelectedPackage
+        {
+            get
+            {
+                return this._selectedPackage;
+            }
+            set
+            {
+                this._selectedPackage = value;
+
+                this.RaisePropertyChanged(() => this.SelectedPackage);
+            }
+        }
 
         public void AddPackageToInstallList(ChocolateyPackageVersion package)
         {
             this.Packages.Add(package);
+
+            this.ClearInstallListCommand.RaiseCanExecuteChanged();
         }
 
         public void RemovePackageToInstallList(ChocolateyPackageVersion package)
         {
             this.Packages.Remove(package);
+
+            this.ClearInstallListCommand.RaiseCanExecuteChanged();
         }
     }
 }
