2f57d0fc9ada498188adf6b94f3deb2b631e661d
James Dibble <james@jdibble.co.uk>
2014-10-27 20:43:31 +0000

    Show package icons in tree view

--- Chocolatey.DomainModel/ChocolateyPackage.cs
+++ Chocolatey.DomainModel/ChocolateyPackage.cs
@@ -1,5 +1,6 @@
 ﻿namespace Chocolatey.DomainModel
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -8,6 +9,8 @@
         public string Id { get; set; }
 
         public string Title { get; set; }
+
+        public Uri IconLink { get { return this.LatestVersion.IconLink; } }
 
         public IEnumerable<ChocolateyPackageVersion> Versions { get; set; }
 
